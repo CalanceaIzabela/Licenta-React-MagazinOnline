2aace2da849f58ca67331ad63a3f8ed22bed4a1f
Izabela Calancea <izabela.calancea@ecenta.com>
2020-05-15 20:23:43 +0300

    CollectionPage implementation

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../components/checkout-item/checkout-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>category</h2>
+  </div>
+);
+
+export default CollectionPage;
